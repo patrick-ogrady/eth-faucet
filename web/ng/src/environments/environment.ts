bd3970f6e9b4e29625c45161aa9492e42903173a
Patrick O'Grady <prohb125@gmail.com>
2022-03-15 00:20:39 -0700

    add ng files

--- web/ng/src/environments/environment.ts
+++ web/ng/src/environments/environment.ts
@@ -6,8 +6,8 @@ import { IEnvironment } from './token';
 
 export const environment: IEnvironment = {
   production: false,
-  apiEndpoint: 'http://127.0.0.1:8000',
-  catpchaSiteKey: '6LcAlacaAAAAACXMQu_VuZup-fAWRbZmxYy5e8xi',
+  apiEndpoint: 'https://faucet-api.trywagmi.xyz',
+  catpchaSiteKey: '6Le4FeAeAAAAAAt5KR7C9clM_kCqbE1P4MPbpMb2',
 };
 
 /*
